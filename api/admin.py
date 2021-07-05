768d6706389fa45b6b1ee788be53fbd8a4816f6c
Andrew <Andrey18651@gmail.com>
2021-07-05 20:22:44 +0300

    add test with flake8

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,6 @@
-from django.contrib import admin
 from .models import Genre, Category, Title, Review, Comment
+from django.contrib import admin
+
 
 admin.site.register(Review)
 admin.site.register(Genre)
